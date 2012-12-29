1902181401795972f13c249782a5e4b2f5b94e81
Justin To <justin.cn.to@gmail.com>
2012-12-29 00:05:33 -0500

    - updated to hardcode the post_id for now to keep things consistent across machines

--- RoveServer/database/populate_tables.sql
+++ RoveServer/database/populate_tables.sql
@@ -6,11 +6,11 @@ INSERT INTO `rove`.`users` (`user_id`, `name`, `email`) VALUES ('3', 'Phong Thie
 
 INSERT INTO `rove`.`users` (`user_id`, `name`, `email`) VALUES ('4', 'Mark Ho', 'mark.sk.ho@gmail.com');
 
-INSERT INTO `rove`.`posts` (`user_id`, `latitude`, `longitude`, `message`) VALUES ('1', '11.1', '11.1', 'Justin was here!');
+INSERT INTO `rove`.`posts` (`post_id`, `user_id`, `latitude`, `longitude`, `message`) VALUES ('1', '1', '11.1', '11.1', 'Justin was here!');
 
-INSERT INTO `rove`.`posts` (`user_id`, `latitude`, `longitude`, `message`) VALUES ('2', '22.2', '22.2', 'Farran was here!');
+INSERT INTO `rove`.`posts` (`post_id`, `user_id`, `latitude`, `longitude`, `message`) VALUES ('2', '2', '22.2', '22.2', 'Farran was here!');
 
-INSERT INTO `rove`.`posts` (`user_id`, `latitude`, `longitude`, `message`) VALUES ('3', '33.3', '33.3', 'Phong was here!');
+INSERT INTO `rove`.`posts` (`post_id`, `user_id`, `latitude`, `longitude`, `message`) VALUES ('3', '3', '33.3', '33.3', 'Phong was here!');
 
-INSERT INTO `rove`.`posts` (`user_id`, `latitude`, `longitude`, `message`) VALUES ('4', '44.4', '44.4', 'Mark was here!');
+INSERT INTO `rove`.`posts` (`post_id`, `user_id`, `latitude`, `longitude`, `message`) VALUES ('4', '4', '44.4', '44.4', 'Mark was here!');
 
